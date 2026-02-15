c1e9880656b4372de715df63db22c447af3d88b2
_prasanth.io/ <204507696+prasanth-io@users.noreply.github.com>
2026-02-16 03:14:00 +0530

    Update theme-toggle.js

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,20 +1,31 @@
 (() => {
-
   const key = 'theme';
   const root = document.documentElement;
-  const toggle = document.getElementById('theme-toggle');
-
+  const light = document.getElementById('theme-light');
+  const system = document.getElementById('theme-system');
+  const dark = document.getElementById('theme-dark');
   const stored = localStorage.getItem(key);
-
-  if (stored) {
-    root.dataset.theme = stored;
-    toggle.checked = stored === 'dark';
+  function applyTheme(theme) {
+    if (theme === 'system') {
+      delete root.dataset.theme;
+    } else {
+      root.dataset.theme = theme;
+    }
   }
-
-  toggle?.addEventListener('change', () => {
-    const theme = toggle.checked ? 'dark' : 'light';
-    root.dataset.theme = theme;
-    localStorage.setItem(key, theme);
+  if (stored === 'light') light.checked = true;
+  else if (stored === 'dark') dark.checked = true;
+  else system.checked = true;
+  applyTheme(stored || 'system');
+  light?.addEventListener('change', () => {
+    localStorage.setItem(key,'light');
+    applyTheme('light');
   });
-
+  dark?.addEventListener('change', () => {
+    localStorage.setItem(key,'dark');
+    applyTheme('dark');
+  });
+  system?.addEventListener('change', () => {
+    localStorage.setItem(key,'system');
+    applyTheme('system');
+  });
 })();
