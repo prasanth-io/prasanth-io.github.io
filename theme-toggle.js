56427191f57601b84d0213e8eae0265df8cbf009
_prasanth.io/ <204507696+prasanth-io@users.noreply.github.com>
2026-02-16 14:45:38 +0530

    Update theme-toggle.js

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,56 +1,32 @@
 (() => {
-
   const key = 'theme';
   const root = document.documentElement;
   const toggle = document.getElementById('theme-toggle');
-
-  const modes = ['system','light','dark'];
-
+  const nextMode = {
+  system: 'light',
+  light: 'dark',
+  dark: 'system'
+  };
   let current = localStorage.getItem(key) || 'system';
-
   const systemQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
   function apply(mode) {
-
-    // update text label
-    toggle.textContent =
+      toggle.textContent =
       mode === 'system' ? 'system theme' : mode;
-
-    // apply theme
-    if (mode === 'system') {
-
+      if (mode === 'system') {
       delete root.dataset.theme;
-
-    } else {
-
+      } else {
       root.dataset.theme = mode;
-
-    }
-
+      }
   }
-
-  // initial load
   apply(current);
-
-  // click cycle
   toggle.addEventListener('click', () => {
-
-    let index = modes.indexOf(current);
-    current = modes[(index + 1) % modes.length];
-
+    current = nextMode[current];
     localStorage.setItem(key, current);
-
     apply(current);
-
-  });
-
-  // auto-update when system theme changes
+    });
   systemQuery.addEventListener('change', () => {
-
     if (current === 'system') {
       apply('system');
     }
-
   });
-
 })();
